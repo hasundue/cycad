882af89313fc8cf97f00eb868df98fb974e3cf04
hasundue <hasundue@gmail.com>
2023-10-19 15:53:53 +0900

    docs(mod): fix module doc

--- mod.ts
+++ mod.ts
@@ -4,10 +4,13 @@
  * ## Example
  *
  * ```ts
- * import { Parser } from "https://deno.land/x/cycad_sitter{version}/mod.ts";
+ * import { Parser } from "https://deno.land/x/cycad@{version}/mod.ts";
  *
- * const parser = await Parser.create("markdown");
- * const tree = parser.parse(`Hello, World!`);
+ * // It may take a while for the first run, because it fetches the parser and
+ * // compiles it to WebAssembly.
+ * const parser = await Parser.create("typescript");
+ *
+ * const tree = parser.parse(`const x = 1;`);
  * console.log(tree.rootNode.toString());
  * ```
  *
